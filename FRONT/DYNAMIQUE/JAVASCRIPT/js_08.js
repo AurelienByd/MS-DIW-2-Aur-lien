e1e2ca0134dcab4d6b2c3e0a742871a163bca335
Aurélien Bayard <rubis91999@hotmail.com>
2023-07-02 10:35:42 +0200

    javascript

--- FRONT/DYNAMIQUE/JAVASCRIPT/js_08.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/js_08.js
@@ -23,17 +23,45 @@
 
 //Exercice 3
 
+//var n = parseInt(window.prompt("Saisir un nombre :"));
+//var s = 0;
+//var m = 0;
 
+//while (n < 0 || n > 0)
+//{
+//    s = s + n;
+//    m++;
+//    n = parseInt(window.prompt("Saisir un nombre :"));
+//}
+//console.log("La somme : " + s);
+//console.log("La moyenne : " + (s/m));
 
 //Exercice 4
 
-let X = window.prompt("Saisir un nombre :");
-let N = window.prompt("Combien de multiples :");
-let R;
+//let X = window.prompt("Saisir un nombre :");
+//let N = window.prompt("Combien de multiples :");
+//let R;
 
-while (N != 0)
-{
-    R = N*X;
-    console.log(N + " x " + X + " = " + R);
-    N--;
-}
+//if (N > 0)
+//{
+//while (N != 0)
+//{
+//    R = N*X;
+//    console.log(N + " x " + X + " = " + R);
+//    N--;
+//}
+//}
+//else if (N < 0)
+//{
+//while (N != 0)
+//{
+//    R = N*X;
+//    console.log(N + " x " + X + " = " + R);
+//    N++;
+//}
+//}
+
+//Exercice 5
+
+var v = window.prompt("Saisir un mot");
+
