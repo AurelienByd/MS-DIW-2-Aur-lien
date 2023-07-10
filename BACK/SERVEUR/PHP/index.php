19d031505bedd59070004ffa838ef7ebbbff6fbd
Aurélien <rubis91999@hotmail.com>
2023-07-10 11:19:39 +0200

    PHP

--- BACK/SERVEUR/PHP/index.php
+++ BACK/SERVEUR/PHP/index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        include("bonjour.php"); 
+    <?php
+        echo $_SERVER["SERVER_ADDR"];
+        echo $_SERVER["REMOTE_ADDR"];
     ?>
 </body>
 </html>
