dca62c91a4cb08a3c61f14f51ff7d206dbfa7608
Aurélien <rubis91999@hotmail.com>
2023-07-11 16:58:15 +0200

    PHP

---
+++ BACK/SERVEUR/PHP/boucles.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // 1. FINI
+    
+    // for ($z = 0; $z <= 150; $z++) { 
+    // if ($z % 2 != 0) {
+    //     echo $z . " ";
+    // }
+    // } 
+
+    // 2. FINI
+
+    // $compte = 1;
+    // $phrase = "Je dois faire des sauvegardes régulières de mes fichiers";
+
+    // while ($compte <= 500) {
+    //     echo $compte . " " . $phrase . " ";
+    //     $compte++;
+    // }
+
+    // 3.
+
+
+    
+
+
+
+
+
+    ?>
+</body>
+</html>
--- BACK/SERVEUR/PHP/index.php
+++ BACK/SERVEUR/PHP/index.php
@@ -6,34 +6,366 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+<?php
 
     // 1. FINI
     
-    // for ($z = 0; $z <= 150; $z++) { 
-    // if ($z % 2 != 0) {
-    //     echo $z . " ";
+    // $tableau = array(
+    //     "Janvier" => 31 . " jours",
+    //     "Février" => 28 . " jours",
+    //     "Mars" => 31 . " jours",
+    //     "Avril" => 30 . " jours",
+    //     "Mai" => 31 . " jours",
+    //     "Juin" => 30 . " jours",
+    //     "Juillet" => 31 . " jours",
+    //     "Août" => 31 . " jours",
+    //     "Septembre" => 30 . " jours",
+    //     "Octobre" => 31 . " jours",
+    //     "Novembre" => 30 . " jours",
+    //     "Décembre" => 31 . " jours"
+    // );
+    // arsort($tableau);
+
+    // foreach($tableau as $cle => $valeur)
+    // {
+    //     echo $cle . " = " . $valeur . "<br>";
     // }
-    // } 
+    
+    // 2.1 FINI
 
-    // 2. FINI
+    // $capitales = array(
+    //     "Bucarest" => "Roumanie",
+    //     "Bruxelles" => "Belgique",
+    //     "Oslo" => "Norvège",
+    //     "Ottawa" => "Canada",
+    //     "Paris" => "France",
+    //     "Port-au-Prince" => "Haïti",
+    //     "Port-d'Espagne" => "Trinité-et-Tobago",
+    //     "Prague" => "République tchèque",
+    //     "Rabat" => "Maroc",
+    //     "Riga" => "Lettonie",
+    //     "Rome" => "Italie",
+    //     "San José" => "Costa Rica",
+    //     "Santiago" => "Chili",
+    //     "Sofia" => "Bulgarie",
+    //     "Alger" => "Algérie",
+    //     "Amsterdam" => "Pays-Bas",
+    //     "Andorre-la-Vieille" => "Andorre",
+    //     "Asuncion" => "Paraguay",
+    //     "Athènes" => "Grèce",
+    //     "Bagdad" => "Irak",
+    //     "Bamako" => "Mali",
+    //     "Berlin" => "Allemagne",
+    //     "Bogota" => "Colombie",
+    //     "Brasilia" => "Brésil",
+    //     "Bratislava" => "Slovaquie",
+    //     "Varsovie" => "Pologne",
+    //     "Budapest" => "Hongrie",
+    //     "Le Caire" => "Egypte",
+    //     "Canberra" => "Australie",
+    //     "Caracas" => "Venezuela",
+    //     "Jakarta" => "Indonésie",
+    //     "Kiev" => "Ukraine",
+    //     "Kigali" => "Rwanda",
+    //     "Kingston" => "Jamaïque",
+    //     "Lima" => "Pérou",
+    //     "Londres" => "Royaume-Uni",
+    //     "Madrid" => "Espagne",
+    //     "Malé" => "Maldives",
+    //     "Mexico" => "Mexique",
+    //     "Minsk" => "Biélorussie",
+    //     "Moscou" => "Russie",
+    //     "Nairobi" => "Kenya",
+    //     "New Delhi" => "Inde",
+    //     "Stockholm" => "Suède",
+    //     "Téhéran" => "Iran",
+    //     "Tokyo" => "Japon",
+    //     "Tunis" => "Tunisie",
+    //     "Copenhague" => "Danemark",
+    //     "Dakar" => "Sénégal",
+    //     "Damas" => "Syrie",
+    //     "Dublin" => "Irlande",
+    //     "Erevan" => "Arménie",
+    //     "La Havane" => "Cuba",
+    //     "Helsinki" => "Finlande",
+    //     "Islamabad" => "Pakistan",
+    //     "Vienne" => "Autriche",
+    //     "Vilnius" => "Lituanie",
+    //     "Zagreb" => "Croatie"
+    // );
+    // ksort($capitales);
 
-    // $compte = 1;
-    // $phrase = "Je dois faire des sauvegardes régulières de mes fichiers";
-
-    // while ($compte <= 500) {
-    //     echo $compte . " " . $phrase . " ";
-    //     $compte++;
+    // foreach($capitales as $cle => $valeur)
+    // {
+    //     echo $cle . " = " . $valeur . "<br>";
     // }
 
-    // 3.
+    // 2.2 FINI
 
+    // $capitales = array(
+    //     "Bucarest" => "Roumanie",
+    //     "Bruxelles" => "Belgique",
+    //     "Oslo" => "Norvège",
+    //     "Ottawa" => "Canada",
+    //     "Paris" => "France",
+    //     "Port-au-Prince" => "Haïti",
+    //     "Port-d'Espagne" => "Trinité-et-Tobago",
+    //     "Prague" => "République tchèque",
+    //     "Rabat" => "Maroc",
+    //     "Riga" => "Lettonie",
+    //     "Rome" => "Italie",
+    //     "San José" => "Costa Rica",
+    //     "Santiago" => "Chili",
+    //     "Sofia" => "Bulgarie",
+    //     "Alger" => "Algérie",
+    //     "Amsterdam" => "Pays-Bas",
+    //     "Andorre-la-Vieille" => "Andorre",
+    //     "Asuncion" => "Paraguay",
+    //     "Athènes" => "Grèce",
+    //     "Bagdad" => "Irak",
+    //     "Bamako" => "Mali",
+    //     "Berlin" => "Allemagne",
+    //     "Bogota" => "Colombie",
+    //     "Brasilia" => "Brésil",
+    //     "Bratislava" => "Slovaquie",
+    //     "Varsovie" => "Pologne",
+    //     "Budapest" => "Hongrie",
+    //     "Le Caire" => "Egypte",
+    //     "Canberra" => "Australie",
+    //     "Caracas" => "Venezuela",
+    //     "Jakarta" => "Indonésie",
+    //     "Kiev" => "Ukraine",
+    //     "Kigali" => "Rwanda",
+    //     "Kingston" => "Jamaïque",
+    //     "Lima" => "Pérou",
+    //     "Londres" => "Royaume-Uni",
+    //     "Madrid" => "Espagne",
+    //     "Malé" => "Maldives",
+    //     "Mexico" => "Mexique",
+    //     "Minsk" => "Biélorussie",
+    //     "Moscou" => "Russie",
+    //     "Nairobi" => "Kenya",
+    //     "New Delhi" => "Inde",
+    //     "Stockholm" => "Suède",
+    //     "Téhéran" => "Iran",
+    //     "Tokyo" => "Japon",
+    //     "Tunis" => "Tunisie",
+    //     "Copenhague" => "Danemark",
+    //     "Dakar" => "Sénégal",
+    //     "Damas" => "Syrie",
+    //     "Dublin" => "Irlande",
+    //     "Erevan" => "Arménie",
+    //     "La Havane" => "Cuba",
+    //     "Helsinki" => "Finlande",
+    //     "Islamabad" => "Pakistan",
+    //     "Vienne" => "Autriche",
+    //     "Vilnius" => "Lituanie",
+    //     "Zagreb" => "Croatie"
+    // );
+    // asort($capitales);
+
+
+    // foreach($capitales as $cle => $valeur)
+    // {
+    //     echo $valeur . " = " . $cle . "<br>";
+    // }
+
+    // 2.3 FINI
+
+    // $capitales = array(
+    //     "Bucarest" => "Roumanie",
+    //     "Bruxelles" => "Belgique",
+    //     "Oslo" => "Norvège",
+    //     "Ottawa" => "Canada",
+    //     "Paris" => "France",
+    //     "Port-au-Prince" => "Haïti",
+    //     "Port-d'Espagne" => "Trinité-et-Tobago",
+    //     "Prague" => "République tchèque",
+    //     "Rabat" => "Maroc",
+    //     "Riga" => "Lettonie",
+    //     "Rome" => "Italie",
+    //     "San José" => "Costa Rica",
+    //     "Santiago" => "Chili",
+    //     "Sofia" => "Bulgarie",
+    //     "Alger" => "Algérie",
+    //     "Amsterdam" => "Pays-Bas",
+    //     "Andorre-la-Vieille" => "Andorre",
+    //     "Asuncion" => "Paraguay",
+    //     "Athènes" => "Grèce",
+    //     "Bagdad" => "Irak",
+    //     "Bamako" => "Mali",
+    //     "Berlin" => "Allemagne",
+    //     "Bogota" => "Colombie",
+    //     "Brasilia" => "Brésil",
+    //     "Bratislava" => "Slovaquie",
+    //     "Varsovie" => "Pologne",
+    //     "Budapest" => "Hongrie",
+    //     "Le Caire" => "Egypte",
+    //     "Canberra" => "Australie",
+    //     "Caracas" => "Venezuela",
+    //     "Jakarta" => "Indonésie",
+    //     "Kiev" => "Ukraine",
+    //     "Kigali" => "Rwanda",
+    //     "Kingston" => "Jamaïque",
+    //     "Lima" => "Pérou",
+    //     "Londres" => "Royaume-Uni",
+    //     "Madrid" => "Espagne",
+    //     "Malé" => "Maldives",
+    //     "Mexico" => "Mexique",
+    //     "Minsk" => "Biélorussie",
+    //     "Moscou" => "Russie",
+    //     "Nairobi" => "Kenya",
+    //     "New Delhi" => "Inde",
+    //     "Stockholm" => "Suède",
+    //     "Téhéran" => "Iran",
+    //     "Tokyo" => "Japon",
+    //     "Tunis" => "Tunisie",
+    //     "Copenhague" => "Danemark",
+    //     "Dakar" => "Sénégal",
+    //     "Damas" => "Syrie",
+    //     "Dublin" => "Irlande",
+    //     "Erevan" => "Arménie",
+    //     "La Havane" => "Cuba",
+    //     "Helsinki" => "Finlande",
+    //     "Islamabad" => "Pakistan",
+    //     "Vienne" => "Autriche",
+    //     "Vilnius" => "Lituanie",
+    //     "Zagreb" => "Croatie"
+    // );
+    // $nbr = sizeof($capitales);
+
+    // echo $nbr . " pays";
+
+    // 2.4
+
+    // $capitales = array(
+    //     "Bucarest" => "Roumanie",
+    //     "Bruxelles" => "Belgique",
+    //     "Oslo" => "Norvège",
+    //     "Ottawa" => "Canada",
+    //     "Paris" => "France",
+    //     "Port-au-Prince" => "Haïti",
+    //     "Port-d'Espagne" => "Trinité-et-Tobago",
+    //     "Prague" => "République tchèque",
+    //     "Rabat" => "Maroc",
+    //     "Riga" => "Lettonie",
+    //     "Rome" => "Italie",
+    //     "San José" => "Costa Rica",
+    //     "Santiago" => "Chili",
+    //     "Sofia" => "Bulgarie",
+    //     "Alger" => "Algérie",
+    //     "Amsterdam" => "Pays-Bas",
+    //     "Andorre-la-Vieille" => "Andorre",
+    //     "Asuncion" => "Paraguay",
+    //     "Athènes" => "Grèce",
+    //     "Bagdad" => "Irak",
+    //     "Bamako" => "Mali",
+    //     "Berlin" => "Allemagne",
+    //     "Bogota" => "Colombie",
+    //     "Brasilia" => "Brésil",
+    //     "Bratislava" => "Slovaquie",
+    //     "Varsovie" => "Pologne",
+    //     "Budapest" => "Hongrie",
+    //     "Le Caire" => "Egypte",
+    //     "Canberra" => "Australie",
+    //     "Caracas" => "Venezuela",
+    //     "Jakarta" => "Indonésie",
+    //     "Kiev" => "Ukraine",
+    //     "Kigali" => "Rwanda",
+    //     "Kingston" => "Jamaïque",
+    //     "Lima" => "Pérou",
+    //     "Londres" => "Royaume-Uni",
+    //     "Madrid" => "Espagne",
+    //     "Malé" => "Maldives",
+    //     "Mexico" => "Mexique",
+    //     "Minsk" => "Biélorussie",
+    //     "Moscou" => "Russie",
+    //     "Nairobi" => "Kenya",
+    //     "New Delhi" => "Inde",
+    //     "Stockholm" => "Suède",
+    //     "Téhéran" => "Iran",
+    //     "Tokyo" => "Japon",
+    //     "Tunis" => "Tunisie",
+    //     "Copenhague" => "Danemark",
+    //     "Dakar" => "Sénégal",
+    //     "Damas" => "Syrie",
+    //     "Dublin" => "Irlande",
+    //     "Erevan" => "Arménie",
+    //     "La Havane" => "Cuba",
+    //     "Helsinki" => "Finlande",
+    //     "Islamabad" => "Pakistan",
+    //     "Vienne" => "Autriche",
+    //     "Vilnius" => "Lituanie",
+    //     "Zagreb" => "Croatie"
+    // );
     
 
+    // 3.1 FINI
+
+    // $valeur = array();
+
+    // $departements = array(
+    //     "Hauts-de-france" => array("Aisne", "Nord", "Oise", "Pas-de-Calais", "Somme"),
+    //     "Bretagne" => array("Côtes d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"),
+    //     "Grand-Est" => array("Ardennes", "Aube", "Marne", "Haute-Marne", "Meurthe-et-Moselle", "Meuse", "Moselle", "Bas-Rhin", "Haut-Rhin", "Vosges"),
+    //     "Normandie" => array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime")
+    // );
+
+    // ksort($departements);
+
+    
+    
+    // foreach ($departements as $cle => $valeur)
+    // {
+    //     foreach ($valeur as $valeur)
+    //     {
+    //     echo $cle . " = " . $valeur . "<br>";
+    //     }
+    // }
+
+    // 3.2
+
+
+    $cle;
+    $valeur1 = array("Aisne", "Nord", "Oise", "Pas-de-Calais", "Somme");
+    $valeur2 = array("Côtes d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan");
+    $valeur3 = array("Ardennes", "Aube", "Marne", "Haute-Marne", "Meurthe-et-Moselle", "Meuse", "Moselle", "Bas-Rhin", "Haut-Rhin", "Vosges");
+    $valeur4 = array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime");
+
+
+    $departements = array(
+        "Hauts-de-france" => $valeur1,
+        "Bretagne" => $valeur2,
+        "Grand-Est" => $valeur3,
+        "Normandie" => $valeur4
+    );
+
+    $nbr;
+    
+
+    foreach ($departements as $cle => $nbr)
+    {
+        $nbr = sizeof($valeur1);
+        echo $nbr . "<br>";
+    }
+    foreach ($departements as $cle => $nbr)
+    {
+        $nbr = sizeof($valeur2);
+        echo $nbr . "<br>";
+    }
+    foreach ($departements as $cle => $nbr)
+    {
+        $nbr = sizeof($valeur3);
+        echo $nbr . "<br>";
+    }
+    foreach ($departements as $cle => $nbr)
+    {
+        $nbr = sizeof($valeur4);
+        echo $nbr . "<br>";
+    }
 
 
 
-
-    ?>
+?>
 </body>
 </html>
