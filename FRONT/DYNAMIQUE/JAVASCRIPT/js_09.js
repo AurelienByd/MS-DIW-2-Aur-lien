f2ac18b903a1aaf2aac1f0bda2348cbc243f5798
Aurélien <rubis91999@hotmail.com>
2023-07-18 16:15:02 +0200

    révisions

--- FRONT/DYNAMIQUE/JAVASCRIPT/js_09.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/js_09.js
@@ -1,12 +1,14 @@
 //Exercice 1
+// var x;
+// var y;
 
-// var x = window.prompt("entrez un nombre :");
-// var y = window.prompt("entrez un multiplicateur :");
-// var p = document.getElementById("a");
-// var q = document.getElementById("b");
 
 // function produit(x, y)
 // {
+//     x = window.prompt("entrez un nombre :");
+//     y = window.prompt("entrez un multiplicateur :");
+//     var p = document.getElementById("a");
+//     var q = document.getElementById("b");
 //     p.innerHTML = "Le cube de " + x + " est égal à " + (x*x*x);
 //     q.innerHTML = "Le produit de " + x + " x " + y + " est égal à " + (x*y);
 //     return;
@@ -16,7 +18,21 @@
 
 //Exercice 2
 
-const str1 = "robert; dupont; amiens; 80600";
+// const str1 = "robert; dupont; amiens; 80600";
+// const str2 =";";
 
-const n = str1.split(";");
-console.log(n[2]);
+// const n = str1.split(str2);
+// console.log(n[2]);
+
+let str1;
+let str2;
+let n;
+
+function tableau(str1, str2, n) {
+    str1 = "robert; dupont; amiens; 80600";
+    str2 =";";
+    n = str1.split(str2);
+    console.log(n[2]);
+}
+
+tableau(str1, str2, n);
